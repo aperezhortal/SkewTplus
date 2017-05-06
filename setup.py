049f62a8c10927d6f4d92d3a339966ed3bf0bcc8
Andres Perez Hortal <andresperezcba@gmail.com>
2017-05-06 01:14:10 +0000

    Update setup files for PyPI

--- setup.py
+++ setup.py
@@ -15,14 +15,27 @@ thermodynamicsLibExtension = Extension( "SkewTplus._thermodynamics",
                                         language='c++')
 
 setup(
-    name='SkewT',
+    name='SkewTplus',
     version='1.1.0',
     author = "Andres Perez Hortal",
     author_email = "andresperezcba@gmail.com",    
     packages=['SkewTplus'],
     ext_modules = cythonize([ thermodynamicsLibExtension]),
-    #url='http://pypi.python.org/pypi/SkewT/',
+    url='http://pypi.python.org/pypi/SkewTplus/',
     license='LICENSE.txt',
     description='Atmospheric Profile Plotting and Diagnostics',
     long_description=open('README.rst').read(),
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Atmospheric Science',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 2'
+    'Programming Language :: Python :: 2.7'
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Cython'],
+    install_requires=['matplotlib','numpy','cython','libgcc'], 
 )
+
+
