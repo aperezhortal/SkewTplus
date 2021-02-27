ce08adef210238f9fcfa282147bdbce75cb970a4
Andres Perez Hortal <aperezhortal@gmail.com>
2021-02-27 11:57:27 -0500

    Run black

--- examples/soundingFromWeb.py
+++ examples/soundingFromWeb.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on Apr 22, 2017
 
 @author: aperez
-'''
+"""
 from SkewTplus.sounding import sounding
 
 
@@ -10,6 +10,5 @@ mySounding = sounding()
 mySounding.fetchFromWeb("20170410:00", "OAX")
 mySounding.quickPlot()
 
-mySounding = sounding("20170410:00",fileFormat='web', stationId= "OAX")
+mySounding = sounding("20170410:00", fileFormat="web", stationId="OAX")
 mySounding.quickPlot()
-
--- examples/soundingQuicView_Save_Image.py
+++ examples/soundingQuicView_Save_Image.py
@@ -1,6 +1,6 @@
-'''
+"""
 Simple SkewT Figure example from Sounding
-'''
+"""
 
 # For python2.7 compatibility
 from __future__ import print_function, division
@@ -8,13 +8,13 @@ from __future__ import print_function, division
 # Load the sounding class from the package
 from SkewTplus.sounding import sounding
 
-#Load the sounding datas
+# Load the sounding datas
 mySounding = sounding("./exampleSounding.txt")
 
 # If no X server is available to visualize the plot, save it to file
 import matplotlib
-matplotlib.use('Agg')
+
+matplotlib.use("Agg")
 
 # Save the SkewT diagram of the Sounding
 mySounding.quickPlot(output_file="quick_sounding.pdf")
-
